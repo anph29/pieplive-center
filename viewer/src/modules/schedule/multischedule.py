560c65a55f3c8952d141f07aea12fa308117e01f
An Phan <aanltll2992@gmail.com>
2019-07-20 16:52:16 +0700

    c
    c

---
+++ viewer/src/modules/schedule/multischedule.py
@@ -0,0 +1,8 @@
+import tkinter as tk
+
+class MultiSchedule(tk.Frame):
+    def __init__(self, parent, *args, **kwargs):
+        super(MultiScheduleLeft, self).__init__()
+
+    def initUI(self):
+        pass
---
+++ viewer/src/modules/schedule/multischeduleright.py
@@ -0,0 +1,8 @@
+import tkinter as tk
+
+class MultiScheduleRigh(tk.Frame):
+    def __init__(self, parent, *args, **kwargs):
+        super(MultiScheduleLeft, self).__init__()
+
+    def initUI(self):
+        pass
